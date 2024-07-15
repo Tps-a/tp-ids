cdb7e349dba83150704dd898680e912865f381bc
jeropc <jeperezc@fi.uba.ar>
2024-07-15 20:55:50 -0300

    actu git.ignore y directorios org

--- back/app.py
+++ back/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from models import db, Auto  # Asegúrate de importar db y Auto correctamente
+from models import obtener_marcas_disponibles
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///autos.db'
@@ -9,6 +10,12 @@ db.init_app(app)
 @app.route('/')
 def main():
     return render_template('main.html')
+
+@app.route('/disena_auto')
+def disena_auto():
+    autos = Auto.query.all() 
+    marcas = obtener_marcas_disponibles() 
+    return render_template('disena_auto.html', autos=autos, marcas=marcas)
 
 @app.route('/autos', methods=['POST'])
 def crear_auto():
@@ -23,13 +30,6 @@ def ver_autos():
     autos = Auto.query.all()  # Recupera todos los autos de la base de datos
     return render_template('disena_auto.html', autos=autos)
 
-@app.route('/disena_auto')
-def disena_auto():
-    # Aquí puedes agregar lógica para pasar datos adicionales a la plantilla si es necesario
-    # Por ejemplo, si quisieras pasar una lista de marcas de autos disponibles para seleccionar
-    # marcas = obtener_marcas_disponibles()
-    # return render_template('disena_auto.html', marcas=marcas)
-    return render_template('disena_auto.html')
 
 if __name__ == '__main__':
     with app.app_context():
--- back/models.py
+++ back/models.py
@@ -7,3 +7,14 @@ class Auto(db.Model):
     marca = db.Column(db.String(80), nullable=False)
     color = db.Column(db.String(50), nullable=False)
     motor = db.Column(db.String(50), nullable=False)
+
+class Marca(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(80), nullable=False)
+    pais = db.Column(db.String(50), nullable=False)
+
+def obtener_marcas_disponibles():
+    # Suponiendo que tienes una clase Marca que representa una tabla de marcas en tu base de datos
+    marcas =   Marca.query.all()  # Obtiene todas las marcas de la base de datos
+    marcas_disponibles = [marca.nombre for marca in marcas]  # Convierte los objetos Marca en una lista de nombres
+    return marcas_disponibles

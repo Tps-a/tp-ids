c484dec4ed678443434da2e6cee2b6c4d67a4db1
jeropc <jeperezc@fi.uba.ar>
2024-07-15 17:16:33 -0300

    models.py creado

---
+++ back/models.py
@@ -0,0 +1,7 @@
+from app import db
+
+class Auto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    marca = db.Column(db.String(50), nullable=False)
+    modelo = db.Column(db.String(50), nullable=False)
+    año = db.Column(db.Integer, nullable=False)

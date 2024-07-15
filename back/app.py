9b9c16c088059929d0860ecba0863e55b4639231
jeropc <jeperezc@fi.uba.ar>
2024-07-15 20:13:23 -0300

    conexion html , tablas

--- back/app.py
+++ back/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from models import db, Auto  # Asegúrate de importar db y Auto correctamente
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///autos.db'
 db.init_app(app)
+
+
+@app.route('/')
+def main():
+    return render_template('main.html')
 
 @app.route('/autos', methods=['POST'])
 def crear_auto():
@@ -12,6 +17,19 @@ def crear_auto():
     db.session.add(nuevo_auto)
     db.session.commit()
     return jsonify({'mensaje': 'Auto creado exitosamente'}), 201
+
+@app.route('/ver_autos')
+def ver_autos():
+    autos = Auto.query.all()  # Recupera todos los autos de la base de datos
+    return render_template('disena_auto.html', autos=autos)
+
+@app.route('/disena_auto')
+def disena_auto():
+    # Aquí puedes agregar lógica para pasar datos adicionales a la plantilla si es necesario
+    # Por ejemplo, si quisieras pasar una lista de marcas de autos disponibles para seleccionar
+    # marcas = obtener_marcas_disponibles()
+    # return render_template('disena_auto.html', marcas=marcas)
+    return render_template('disena_auto.html')
 
 if __name__ == '__main__':
     with app.app_context():
--- js/main1.js
+++ js/main1.js
@@ -1,11 +1,13 @@
-const sidebar = document.getElementById('sidebar');
-
 document.addEventListener('mousemove', (event) => {
     if (event.clientX > window.innerWidth - 200) {
         sidebar.classList.remove('hidden');
         sidebar.classList.add('show');
+       
+        document.getElementById('sidebarIndicator').style.display = 'none';
     } else {
         sidebar.classList.remove('show');
         sidebar.classList.add('hidden');
+       
+        document.getElementById('sidebarIndicator').style.display = 'block';
     }
 });

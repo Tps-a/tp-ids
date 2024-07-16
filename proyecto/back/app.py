c4861a91d92bf776d9b788ea64abc422343d016d
leannnica <laloisi@fi.uba.ar>
2024-07-16 16:47:12 -0300

    Implemento rutas en flask para enviar archivos html, css y js.

--- proyecto/back/app.py
+++ proyecto/back/app.py
@@ -1,16 +1,19 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, send_from_directory, render_template
 from models import db, Auto, Marca #se pueden importar mas cosas 
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../front/HTML')
 port= 5000
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tu_base_de_datos.db' ## crear base de datos
 ## crear base de datos con posgrest y poner la url de la base de datos arriba 
 
 @app.route('/')
 def home():
-    with open('/proyecto/front/HTML/main.html', 'r') as html_file:
-        html_content = html_file.read()
-    return make_response(html_content, 200, {'Content-Type': 'text/html'})
+    return render_template('main.html')
+
+@app.route('/disenar')
+def disenar():
+    return render_template('disena_auto.html')
 
 @app.route('/crear-auto', methods=['POST']) 
 def crear_auto():
@@ -46,6 +49,21 @@ def eliminar_auto(auto_id):
     db.session.delete(auto)
     db.session.commit()
     return jsonify({'mensaje': 'Auto eliminado exitosamente'})
+
+@app.route('/css/<path:filename>')
+def enviar_css(filename):
+    css_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'front', 'css'))
+    return send_from_directory(css_dir, filename)
+
+@app.route('/javascript/<path:filename>')
+def enviar_js(filename):
+    js_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'front', 'java_script'))
+    return send_from_directory(js_dir, filename)
+
+@app.route('/modelos/<path:filename>')
+def enviar_modelos(filename):
+    models_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'front', 'modelos'))
+    return send_from_directory(models_dir, filename)
 
 #investigar esto si estas viendo este comentario
 # Asegúrate de tener una función initialize_app() definida en algún lugar

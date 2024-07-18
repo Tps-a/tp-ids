689c82db9b401c070d9ed76c6261620c95b2817c
leannnica <laloisi@fi.uba.ar>
2024-07-18 17:09:09 -0300

    Cambios en funcion login y creacion de funcion register.

--- proyecto/front/java_script/inicio.js
+++ proyecto/front/java_script/inicio.js
@@ -1,18 +1,61 @@
+function register(event) {
+    event.preventDefault();
+    let usuario_nuevo = document.getElementById("usuario").value
+    let password_nuevo = document.getElementById("password").value
+    
+    fetch("http://localhost:5000/registrarse", 
+        {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                usuario: usuario_nuevo,
+                password: password_nuevo
+            })
+        }
+    )
+    .then(response => {
+        if (!response.ok) {
+            return response.json().then(data => {
+                alert(data.error);
+                throw new Error(data.error);
+            });
+        }
+        return response.json()
+    })
+    .then(data => {
+        alert(data.mensaje);
+        window.location.href = '/login_page'
+    })
+}
+
 function login(event) {
     event.preventDefault()
-    let usuario_nuevo = document.getElementById("usuario").value;
-    let password_nuevo = document.getElementById("password").value;
-    fetch("http://localhost:5000/loguearse", 
-        {method: "POST" , 
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify( {
-            usuario: usuario_nuevo,
-            password: password_nuevo,
-            } )
-        } )
+    let usuario = document.getElementById("usuario").value
+    let password = document.getElementById("password").value
 
-        .then(response => response.json())
-        .then(data => {
-            console.log("respuesta: ",data.mensaje);
-        })
+    fetch("http://localhost:5000/login", 
+        {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                usuario: usuario,
+                password: password
+            })
+        }
+    )
+    .then(response => {
+        if (!response.ok) {
+            return response.json().then(data => {
+                alert(data.error)
+                throw new Error(data.error)
+            });
+        }
+        return response.json()
+    })
+    .then(data => {
+        window.location.href = `/main/${data.usuario_id}`
+    })
+    .catch(error => {
+        console.error('Error:', error)
+    })
 }
